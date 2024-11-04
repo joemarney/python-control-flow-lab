972fddf703d7010cd08340c0af04daf4ea8eb092
joemarney <marney.joe@gmail.com>
2024-11-04 16:31:35 +0000

    level up completed

--- exercise.py
+++ exercise.py
@@ -221,4 +221,60 @@ def determine_season():
     except:
         print('Invalid month and/or date. Example: May 24')
 # Call the function
-determine_season()
+# determine_season()
+
+
+# Exercise 7: Number Guessing Game
+#
+# Write a Python function named `guess_number` that allows a user to guess a predetermined number within a range.
+#
+# Requirements:
+# - Set a fixed number as the target for guessing (e.g., 42).
+# - Prompt the user to guess a number within a range (e.g., 1 to 100).
+# - Allow the user to guess up to five times.
+# - After each guess, use conditional statements with AND, OR, and NOT to give the user hints like:
+#   - "Guess is too low" or "Guess is too high."
+#   - "Last chance!" when they are on their fifth guess.
+# - Print "Congratulations, you guessed correctly!" if they guess the number.
+# - Print "Sorry, you failed to guess the number in five attempts." if they do not succeed.
+#
+# Hints:
+# - Use a for loop with a range to limit guesses to five.
+# - Use logical AND, OR, and NOT to check conditions and provide appropriate feedback.
+
+def guess_number():
+    # Your control flow logic goes here
+    number = 42
+    guesses = 5
+    turns = 1
+    win = False
+    # answer = int(input('Guess a number between 1 and 100: '))
+
+    # if answer > number:
+    #     print('Too High!')
+    # if answer < number:
+    #     print('Too Low!')
+
+    # if answer == number:
+    #     print('Congratulations, you guessed correctly!')
+    while win is False:
+        if turns < guesses:
+            guess = int(input('Guess a number between 1 and 100: '))
+            if guess > number:
+                print('Too High!')
+            elif guess < number:
+                print('Too Low!')
+            else:
+                print('Congratulations, you guessed correctly!')
+                break
+            turns += 1
+        elif turns == guesses:
+            guess = int(input('Last Chance! Guess a number between 1 and 100: '))
+            if guess != number:
+                print('Sorry, you failed to guess the number in five attempts.')
+            if guess == number:
+                print('Congratulations, you guessed correctly!')
+            break
+# Call the function
+guess_number()
+
